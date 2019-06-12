7d7b0d69d875103dc23b4fe382abcd135c1261e5
Tushar Sondhi <tusharsondhiepic@gmail.com>
2019-06-11 22:12:00 -0400

    Properly cleaned up and ready to pack

--- neural_network.py
+++ neural_network.py
@@ -203,7 +203,7 @@ class NeuralNetwork:
 
         # converts target to target
         # First list is the input
-        layers = [np.array([input_list])]
+        layers = [np.array([input_list]).T]
         for weight_layer in self.weights:
 
             # multiplies previous layer with the weights to get the next layer
@@ -223,32 +223,35 @@ class NeuralNetwork:
         # Finds the Output error
         output_error = targets - output
 
+        weights_len = len(self.weights)
         # loop to create a list of numpy arrays of errors in reverse order (i.e. output error is first error)
         error_array = []
-        for i in range(0, len(self.weights)):
-            if i == 1:
+        for i in range(0, weights_len):
+            if i == 0:
                 # first error is output error
                 error_array += [output_error]
             else:
                 # print(error_array)
-                error_array += [np.dot(self.weights[-i + 1].T, output_error)]
-        #print(len(error_array))
+                error_array += [np.dot(self.weights[weights_len - i].T, error_array[i - 1])]
+        #print(error_array)
         # Backpropogation loop
-        weights_len = len(self.weights)
 
-        b = np.dot((error_array[1] * layers[2] * (1.0 - layers[2])), np.transpose(layers[1]))
-        self.weights[1] += self.learning_rate * b
-        b = np.dot((error_array[0] * layers[1] * (1.0 - layers[1])), layers[0])
-        self.weights[0] += self.learning_rate * b
-        #print(1)
-        # for i in range(0, weights_len):
-        #     # Applies backpropogation function to weights matrix
-        #     #print(len(error_array[-i - 1]))
-        #     #print(len(layers[-i - 1]))
-        #     a = error_array[i - 1] * layers[-i - 1] * (1.0 - layers[-i - 1])
-        #     prop_dot_product = np.dot((error_array[i - 1] * layers[-i - 1] * (1.0 - layers[-i - 1])), np.transpose(layers[i]))
-        #     self.weights[-i - 1] += self.learning_rate * prop_dot_product
-        #     print(1)
+
+        # b = np.dot((error_array[0] * layers[2] * (1.0 - layers[2])), np.transpose(layers[1]))
+        # self.weights[1] += self.learning_rate * b
+        # b = np.dot((error_array[1] * layers[1] * (1.0 - layers[1])), np.transpose(layers[0]))
+        # self.weights[0] += self.learning_rate * b
+
+        for i in range(0, weights_len):
+            # Applies backpropogation function to weights matrix
+            #print(len(error_array[-i - 1]))
+            #print(len(layers[-i - 1]))
+            a = error_array[i] * layers[-i - 1] * (1.0 - layers[-i - 1])
+            prop_dot_product = np.dot(a, np.transpose(layers[weights_len - i - 1]))
+            self.weights[-i - 1] += self.learning_rate * prop_dot_product
+            #print(1)
+
+
 def showData():
     # Opens file of training data and compiles a list of all the training dat
     data_file = open("mnist_dataset/mnist_train.csv", 'r')
@@ -274,9 +277,9 @@ def trainAndTestMNISTDataset(epochs):
 
     # Initializes a neural network with 784 input nodes (for each of the pixels in the image) and 10 ending nodes
     # (one for each of the possibilities 0-9 for the handwritten letters)
-    nn = NeuralNetwork([784, 100, 10], 0.2)
+    nn = NeuralNetwork([784, 200, 100, 10], 0.5)
     count = 0
-    # For loop runs through each set of training data
+    # For loop runs through each  set of training data
     for _ in range(epochs):
         for string_data in data_list:
 
@@ -325,6 +328,7 @@ def trainAndTestMNISTDataset(epochs):
         # print(n_output)
 
     print(count / 10000)
+
 
 trainAndTestMNISTDataset(1)
 #showData()
